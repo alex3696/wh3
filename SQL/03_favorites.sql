3e95c7718ab4295eecbf1a57d173daa9db434420
alex3696 <alex3696@yandex.ru>
2015-08-16 16:06:01 +0500

    Переработал избранное

--- SQL/03_favorites.sql
+++ SQL/03_favorites.sql
@@ -1,33 +1,45 @@
 ﻿DROP TABLE IF EXISTS t_favorites CASCADE;
-DROP TABLE IF EXISTS t_whclass_prop CASCADE;
 
 -----------------------------------------------------------------------------------------------------------------------------
 -- таблица содержащая иерархическую структуру избранного для пользователей 
 -----------------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE t_favorites ( 
-	favorites_id	SERIAL 	PRIMARY KEY		
-	,pid		INTEGER NOT NULL DEFAULT 1 REFERENCES t_favorites(favorites_id) 	MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE --SET DEFAULT
-	,owner		NAME	NOT NULL DEFAULT CURRENT_USER REFERENCES wh_role(rolname) 	MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
-	,expanded	BOOLEAN	NOT NULL DEFAULT TRUE
-	,label		TEXT 	 DEFAULT NULL 
-	,CONSTRAINT  ck_favorites_root CHECK ( (favorites_id>0 AND pid>0 AND favorites_id<>pid) OR (favorites_id=0 AND pid=0) OR (favorites_id=1 AND pid=0) )-- один корень
+  id           SERIAL  PRIMARY KEY
+ ,pid          INTEGER NOT NULL DEFAULT 1 
+   REFERENCES t_favorites(id)    MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE --SET DEFAULT
+ ,owner        NAME    NOT NULL DEFAULT CURRENT_USER 
+   REFERENCES wh_role(rolname)   MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
+ ,expanded     BOOLEAN NOT NULL DEFAULT TRUE
+ ,label        TEXT    DEFAULT NULL 
 
-	,classid 	INTEGER	 DEFAULT NULL REFERENCES  wh_class(classid) 	MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
-	,objnumid 	INTEGER	 DEFAULT NULL REFERENCES  wh_objnum(id) 	MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
+ ,node_type   SMALLINT NOT NULL DEFAULT 0 CHECK (node_type>=0 AND node_type<3)
+ ,cls_id      INTEGER  DEFAULT NULL REFERENCES  t_cls(id)   MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
+ ,obj_id      INTEGER  DEFAULT NULL REFERENCES  t_objnum(id)MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
+
+
+,CONSTRAINT ck_fav_type CHECK (  (node_type=0 AND cls_id IS NULL AND obj_id IS NULL)
+                               OR(node_type=1 AND cls_id IS NOT NULL AND obj_id IS NULL)
+                               OR(node_type=2 AND cls_id IS NULL AND obj_id IS NOT NULL))
+ 
+,CONSTRAINT ck_fav_root CHECK (  (id=0 AND pid=0) -- main root
+                               OR(id=1 AND pid=0) -- Object0
+                               OR(id>1 AND pid>0 AND id<>pid)   )  -- один корень
+
 );
-GRANT SELECT ON TABLE t_favorites  			TO "Guest";
-GRANT INSERT,DELETE,UPDATE ON TABLE t_favorites 	TO "User";
-GRANT USAGE ON TABLE t_favorites_favorites_id_seq	TO "User"; 		-- Позволяет читать,инкрементировать текущее значение очереди
+GRANT SELECT ON TABLE t_favorites  TO "Guest";
+GRANT INSERT,DELETE,UPDATE ON TABLE t_favorites TO "User";
+GRANT USAGE ON TABLE t_favorites_id_seq TO "User"; -- Позволяет читать,инкрементировать текущее значение очереди
+SELECT setval('t_favorites_id_seq', 100); 
 
-INSERT INTO t_favorites(favorites_id,pid,label)   VALUES (0,0,'root');
-INSERT INTO t_favorites(pid,label)   VALUES (0,'virtual_root'); 		-- favorites_id=1
+INSERT INTO t_favorites(id,pid,label)   VALUES (0,0,'root');
+INSERT INTO t_favorites(id,pid,label)   VALUES (1,0,'virtual_root');
 
-INSERT INTO t_favorites(pid,label)   VALUES (1,'folder_1');
-INSERT INTO t_favorites(pid,label)   VALUES (1,'folder_2');
-INSERT INTO t_favorites(pid,label)   VALUES (1,'folder_3');
-INSERT INTO t_favorites(pid,classid)   VALUES (2,103);
-INSERT INTO t_favorites(pid,objnumid)   VALUES (3,11);
+INSERT INTO t_favorites(pid,label,node_type) VALUES (1,'folder_1',0);
+INSERT INTO t_favorites(pid,label,node_type) VALUES (1,'folder_2',0);
+INSERT INTO t_favorites(pid,label,node_type) VALUES (1,'folder_3',0);
+INSERT INTO t_favorites(pid,cls_id,node_type)VALUES (102,103,1);
+INSERT INTO t_favorites(pid,obj_id,node_type)VALUES (103,103,2);
 
 -----------------------------------------------------------------------------------------------------------------------------
 -- Вьюшка с сылками избранного
@@ -35,27 +47,36 @@ INSERT INTO t_favorites(pid,objnumid)   VALUES (3,11);
 --DROP VIEW w_favorites;
 CREATE OR REPLACE VIEW w_favorites AS 
 SELECT 
-	 t_favorites.favorites_id
-	,t_favorites.pid		
-	,t_favorites.owner		
-	,t_favorites.expanded	
-	,t_favorites.label	
-	,t_favorites.classid	
-	,t_favorites.objnumid	
-	
+  t_favorites.id
+ ,t_favorites.pid
+ ,t_favorites.owner
+ ,t_favorites.expanded
+ ,t_favorites.label
+ ,t_favorites.node_type
 
-	,wh_objnum.objname 	AS objname
-	,wh_objnum.classname 	AS objclass
+ ,t_favorites.cls_id
+ ,t_cls.label AS favcls_label
+ ,t_cls.type AS favcls_type
+ ,t_cls.pid AS favcls_pid
+ ,t_cls.measurename AS favcls_mes
 
-	,wh_class.classname	AS classfilter
-
-FROM
-	t_favorites
-	LEFT JOIN wh_class  ON t_favorites.classid =wh_class.classid 
-	LEFT JOIN wh_objnum ON t_favorites.objnumid=wh_objnum.id
+ ,t_favorites.obj_id
+  ,t_objnum.obj_label AS favobj_label
+  ,t_objnum.pid AS favobj_pid
+/*
+ ,objcls.label AS favobj_cls_label
+ ,objcls.type AS favobj_cls_type
+ ,objcls.pid AS favobj_cls_pid
+ ,objcls.measurename AS favobj_cls_mes
+*/
+FROM t_favorites
+  LEFT JOIN t_objnum ON t_favorites.obj_id=t_objnum.id
+  LEFT JOIN t_cls  ON t_favorites.cls_id =t_cls.id 
+  OR t_objnum.cls_id=t_cls.id 
+--  LEFT JOIN t_cls objcls  ON t_objnum.cls_id =objcls.id 
 WHERE t_favorites.owner=CURRENT_USER;
 
-GRANT SELECT ON TABLE w_favorites  			TO "Guest";
+GRANT SELECT ON TABLE w_favorites  TO "Guest";
 
 
 /**
@@ -84,52 +105,9 @@ select $1 ~ ''^(-)?[\d]+((\.)[\d]+)?((e|E)(-)?[\d]+)?$'' as result
 --SELECT isnumber('6.6E6')
 
 
------------------------------------------------------------------------------------------------------------------------------
--- таблица свойств классов
------------------------------------------------------------------------------------------------------------------------------
-
-CREATE TABLE t_whclass_prop ( 
-	classid 	INTEGER	NOT NULL REFERENCES  wh_class(classid) 	MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE
-	,label		TEXT 	NOT NULL
-	,val		TEXT
-	,type 		SMALLINT NOT NULL DEFAULT 0 -- 0=text, 1=num, 2=date, 3=link, 4=file
-	,time 		TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-	,editor 	NAME NOT NULL DEFAULT "current_user"()
-	,prop_id	SERIAL 	UNIQUE		
-
-	,CONSTRAINT pk_t_whclass_prop		PRIMARY KEY(classid,label)
-	,CONSTRAINT ck_t_whclass_prop_type 	CHECK ( (type=1 AND isnumber(val)) OR type<>1  )
-
-);
-GRANT SELECT ON TABLE t_whclass_prop  			TO "Guest";
-GRANT INSERT,DELETE,UPDATE ON TABLE t_whclass_prop 	TO "Admin";
-
-
-
-
--- Свойства базы данных --
-INSERT INTO t_whclass_prop(classid, label, val)   	VALUES (2, 'FTP server', '192.168.0.18');
-INSERT INTO t_whclass_prop(classid, label, val)		VALUES (2, 'FTP port', '21');
-INSERT INTO t_whclass_prop(classid, label, val)		VALUES (2, 'FTP username', 'postgres');
-INSERT INTO t_whclass_prop(classid, label, val)		VALUES (2, 'FTP password', '1');
-
--- Тестовые cвойства --
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '12', 12, 1);
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '1', 1, 1);
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '11', 11, 1);
-INSERT INTO t_whclass_prop(classid, label, val)         VALUES (103, 'str', 'str');
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '21', 21, 1);
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '3', 3, 1);
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, '2', 2, 1);
-INSERT INTO t_whclass_prop(classid, label, val, type)   VALUES (103, 'c2', 'c2', 0);
---SELECT label,val, CASE  WHEN isnumber(val) THEN val::NUMERIC ELSE NULL END AS _data  FROM t_prop_class ORDER BY _data 
 
 
 
 
 
 
-
-
-
-	
