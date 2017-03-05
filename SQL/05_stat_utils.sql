65ddc1672e22601616cdd0102abba6957c7908ff
alex3696 <alex3696@yandex.ru>
2017-03-05 15:27:36 +0500

    SQL: исправленs статистики местонахождения (если нет статистики - объект считается несуществующим на данном интервале ) , выделен подсчёт местонахождения прибора в основную систему

---
+++ SQL/05_stat_utils.sql
@@ -0,0 +1,128 @@
+BEGIN TRANSACTION;
+
+--SET client_min_messages = 'error';
+--SET client_min_messages = 'warning';
+SET client_min_messages = 'notice';
+--SET client_min_messages = 'debug';
+--SHOW client_min_messages;
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+/**
+DROP FUNCTION IF EXISTS stat_get_obj_location(_oid BIGINT,_begin TIMESTAMP) CASCADE;
+CREATE OR REPLACE FUNCTION stat_get_obj_location(_oid BIGINT,_begin TIMESTAMP) 
+  RETURNS BIGINT AS
+$BODY$ 
+  SELECT CASE WHEN dst_path[1][2] IS NOT NULL THEN dst_path[1][2] ELSE src_path[1][2] END AS obj_parent_oid
+      FROM log_main 
+      LEFT JOIN log_detail_move USING (id)
+      WHERE obj_id = _oid 
+        AND timemark < _begin 
+      ORDER BY timemark DESC
+      FETCH FIRST 1 ROW ONLY --LIMIT 1
+$BODY$
+LANGUAGE sql STABLE COST 10;
+GRANT EXECUTE ON FUNCTION stat_get_obj_location(_oid BIGINT,_begin TIMESTAMP) TO "Guest";
+
+SELECT * FROM stat_get_obj_location(3883,'2015.02.10 00:00+5');
+SELECT * FROM stat_get_obj_location(3883,'2015.04.10 00:00+5');
+SELECT * FROM stat_get_obj_location(3883,'2017.04.10 00:00+5');
+*/
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+DROP FUNCTION IF EXISTS stat_mov_obj_all(_oid BIGINT, _begin TIMESTAMP, _end TIMESTAMP) CASCADE;
+CREATE OR REPLACE FUNCTION stat_mov_obj_all(_oid BIGINT, _begin TIMESTAMP, _end TIMESTAMP) 
+ RETURNS TABLE( dst_time INTERVAL
+               ,dst_oid  BIGINT
+              ) AS $BODY$ 
+DECLARE 
+  _olog CURSOR(_start TIMESTAMP, _stop TIMESTAMP) IS
+    SELECT log_dt, src_oid, log.dst_oid, mobj_id, mcls_id FROM log 
+      WHERE mobj_id= _oid 
+        AND act_id IS NULL
+        AND log_dt>=_start AND log_dt<=_stop 
+    ORDER BY log_dt ASC
+    ;
+  rec           RECORD;
+  curr_dt       TIMESTAMP;
+  curr_dst_oid  BIGINT;
+
+  _location_cursor CURSOR IS
+      SELECT CASE WHEN log.dst_oid IS NOT NULL THEN log.dst_oid ELSE log.src_oid END 
+      FROM log WHERE mobj_id = _oid 
+    AND log_dt < _begin  
+    ORDER BY log_dt DESC 
+    
+    ;
+BEGIN
+
+-- пытаемся найти первое упоминание до диапазона 
+--  SELECT log.act_id,log.dst_oid,log.src_oid INTO rec FROM log WHERE mobj_id = _oid 
+--    AND log_dt < _begin  -- AND log_dt>'2000.01.01 00:00'
+--    ORDER BY log_dt DESC 
+--  LIMIT 1; ??? почемуто выполняется медленно если объект создан внутри диапазона !!ЕСЛИ ЕСТЬ ЛИМИТ!!
+
+--    SELECT CASE WHEN log.dst_oid IS NOT NULL THEN log.dst_oid ELSE log.src_oid END 
+--      INTO curr_dst_oid FROM log WHERE mobj_id = _oid 
+--    AND log_dt < _begin  
+--    ORDER BY log_dt DESC 
+--    LIMIT 1 --FETCH FIRST 1 ROW ONLY
+--    ; 
+
+  -- перенос LIMIT в функцию работает так же 
+  --SELECT stat_get_obj_location(_oid,_begin) INTO curr_dst_oid; 
+
+  OPEN  _location_cursor;
+  FETCH _location_cursor INTO curr_dst_oid;
+  CLOSE _location_cursor;
+
+  IF curr_dst_oid IS NOT NULL THEN 
+    curr_dt :=_begin; 
+    --RAISE NOTICE  'объект существовал до диапазона' ;
+  ELSE
+    -- ищем первое упоминание внутри диапазона
+    SELECT src_oid,log_dt INTO rec FROM log 
+      WHERE mobj_id= _oid 
+        AND log_dt>=_begin AND log_dt<=_end
+      ORDER BY log_dt ASC
+      LIMIT 1;
+    IF rec.log_dt IS NOT NULL AND rec.src_oid IS NOT NULL THEN 
+      --RAISE NOTICE  'объект создан внутри диапазона' ;
+      curr_dt :=rec.log_dt; 
+      curr_dst_oid:= rec.src_oid;
+    ELSE 
+      --RAISE NOTICE  'внутри диапазона нет сведений - выход' ;
+      RETURN;
+    END IF;
+  END IF;
+
+  OPEN _olog(_begin,_end);
+  LOOP
+    FETCH _olog INTO rec;
+    IF rec.log_dt IS NOT NULL THEN
+      dst_time := rec.log_dt - curr_dt;
+      dst_oid  := curr_dst_oid;
+      RETURN NEXT;
+      curr_dst_oid := rec.dst_oid;
+      curr_dt      := rec.log_dt;
+    ELSE
+      dst_time := _end - curr_dt;
+      dst_oid  := curr_dst_oid;
+      RETURN NEXT;
+      EXIT;
+    END IF;
+  END LOOP;
+  CLOSE _olog;
+
+  RETURN;
+END; 
+$BODY$ LANGUAGE plpgsql STABLE COST 50 ROWS 50;
+GRANT EXECUTE ON FUNCTION stat_mov_obj_all(BIGINT,TIMESTAMP,TIMESTAMP) TO "Guest";
+/**
+SELECT * FROM log WHERE mobj_id= 3883 ORDER BY log_dt DESC;
+SELECT * FROM stat_mov_obj_all (3883,'2015.01.01 00:00+5','2015.07.01 00:00+5');
+*/
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+COMMIT TRANSACTION;
--- SQL/stat_mov_cls.sql
+++ SQL/stat_mov_cls.sql
@@ -1,102 +1,5 @@
 
 
-DROP FUNCTION IF EXISTS pg_temp.stat_mov_obj_all(_oid BIGINT, _begin TIMESTAMP, _end TIMESTAMP) CASCADE;
-CREATE OR REPLACE FUNCTION pg_temp.stat_mov_obj_all(_oid BIGINT, _begin TIMESTAMP, _end TIMESTAMP) 
- RETURNS TABLE( cid      BIGINT 
-               ,oid      BIGINT
-               ,dst_time INTERVAL
-               ,dst_oid  BIGINT
-              ) AS $BODY$ 
-  DECLARE 
-    --curr_dt TIMESTAMP;
-    --curr_place BIGINT;
-    prev_dt TIMESTAMP;
-    prev_place BIGINT;
-    prev_src_oid BIGINT;
-    rec RECORD;
-
-    _olog CURSOR(_start TIMESTAMP, _stop TIMESTAMP, _oid  BIGINT ) IS
-    SELECT log_dt, src_oid, log.dst_oid, mobj_id, mcls_id FROM log WHERE mobj_id= _oid 
-    AND act_id IS NULL
-    AND log_dt<_stop AND log_dt>_start
-    ORDER BY log_dt DESC
-    ;
-BEGIN
-  OPEN _olog(_begin,_end,_oid);
-  FETCH _olog INTO rec;
-  
-  oid := _oid;
-  prev_dt:= _end;
-
-  IF rec.log_dt IS NULL THEN
-    SELECT log.dst_oid,log.mcls_id INTO prev_src_oid,cid
-    FROM log WHERE mobj_id= _oid 
-      AND act_id IS NULL
-      AND log_dt<=_begin
-      ORDER BY log_dt DESC LIMIT 1;
-    IF prev_src_oid IS NULL OR cid IS NULL THEN 
-      SELECT obj.pid,obj.cls_id INTO prev_src_oid,cid FROM obj WHERE id=_oid;
-    END IF;
-  ELSE
-    cid          := rec.mcls_id;
-    prev_src_oid := rec.src_oid;
-    prev_place   := rec.dst_oid;
-  END IF;
-  --RAISE NOTICE  'rec=%', rec;
-  --IF rec IS NOT NULL THEN RAISE NOTICE  'NOT NULL'; ELSE RAISE NOTICE  'IS NULL'; END IF;
-  WHILE prev_src_oid IS NOT NULL LOOP
-    --RAISE NOTICE  'prev_dt=%   prev_place=%',prev_dt,prev_place ;
-    
-    --RAISE NOTICE  'curr_dt=%   curr_place=%',curr_dt,curr_place ;
-    IF rec.log_dt IS NOT NULL THEN 
-      dst_time := prev_dt-rec.log_dt;
-      dst_oid := rec.dst_oid;
-      RETURN NEXT;
-    ELSE
-      --RAISE NOTICE  'LAST NS curr_dt=%   curr_place=%',curr_dt,curr_place ;
-      dst_time := prev_dt-_begin;
-      dst_oid := prev_src_oid;
-      RETURN NEXT;
-      EXIT;
-    END IF;
-    prev_src_oid:=rec.src_oid;
-    prev_dt:=rec.log_dt;
-    prev_place:=rec.dst_oid;
-    FETCH _olog INTO rec;
-  END LOOP;
-  CLOSE _olog;
-
-  RETURN;
-END; 
-$BODY$ LANGUAGE plpgsql STABLE  COST 1000 ROWS 1000;
-
-/**
-SELECT * FROM pg_temp.stat_mov_obj_all (3626,'2016.09.10 12:00+5','2017.02.27 20:00+5');
-
-SELECT * FROM pg_temp.place_time (3934,'2017.01.01 00:00+5','2017.02.27 20:00+5');
-
-SELECT *,obj_name.title FROM pg_temp.place_time (3782,'2017.01.31 00:00+5','2017.02.27 20:00+5') ss
-LEFT  JOIN obj_name ON obj_name.id=ss.dst_oid
-
-SELECT *,obj_name.title FROM pg_temp.place_time (3782,'2014.10.01 00:00+5','2014.11.30 20:00+5') ss
-LEFT  JOIN obj_name ON obj_name.id=ss.dst_oid
-
-
-    SELECT log_dt, src_oid, log.dst_oid, mobj_id, mcls_id FROM log WHERE mobj_id= 3782 
-    AND act_id IS NULL
-    AND log_dt<'2017.02.27 20:00+5' AND log_dt>'2017.01.01 00:00+5'
-    ORDER BY log_dt DESC
-
-
-select SUM(dst_time),dst_oid,cid,oid 
-  from pg_temp.place_time (3626,'2016.09.10 12:00+5','2017.02.27 20:00+5')
-  GROUP BY dst_oid,cid,oid 
-
-*/
--------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------
-
 DROP FUNCTION IF EXISTS pg_temp.stat_mov_cls() CASCADE;
 CREATE OR REPLACE FUNCTION pg_temp.stat_mov_cls() 
  RETURNS VOID
@@ -113,9 +16,9 @@ DECLARE
     SELECT * FROM obj WHERE cls_id =in_cid
   ;
   _stat_cursor CURSOR( _oid  BIGINT, _start TIMESTAMP, _stop TIMESTAMP) IS
-    select SUM(ostat.dst_time) AS dst_time,ostat.dst_oid,ostat.cid,ostat.oid 
-    from pg_temp.stat_mov_obj_all (_oid,_start,_stop) ostat
-    GROUP BY ostat.dst_oid,ostat.cid,ostat.oid 
+    select SUM(ostat.dst_time) AS dst_time,ostat.dst_oid
+    from stat_mov_obj_all (_oid,_start,_stop) ostat
+    GROUP BY ostat.dst_oid
   ;
   _begin TIMESTAMP;
   _end TIMESTAMP;
@@ -126,6 +29,8 @@ DECLARE
   mov_row   RECORD;
   curr_perc NUMERIC;
   col_name  NAME;
+
+  obj_exists BOOLEAN;
 BEGIN 
   --_begin := '?Начало периода?DATE?2017.01.01? 00:00:00'::TIMESTAMP ;
   --_end := '?Конец периода?DATE?2017.12.31? 23:59:56'::TIMESTAMP ;
@@ -165,14 +70,16 @@ BEGIN
     -- iterate OBJECTS in CLS
     FOR obj_row IN _obj_cursor( cls_row.cid) LOOP
       -- iterate STATISTIC for OBJECTS in CLS
-      curr_oqty:= curr_oqty+obj_row.qty;
+      obj_exists:=FALSE;
       FOR stat_row IN _stat_cursor( obj_row.id,_begin,_end) LOOP
+        obj_exists:=TRUE;
         INSERT INTO _sum_dst_time_cls (cid, dst_oid,sum_dst_time)
           VALUES (cls_row.cid, stat_row.dst_oid, stat_row.dst_time)
           ON CONFLICT ON CONSTRAINT pk_sum_dst_time_cls
           DO UPDATE SET sum_dst_time = _sum_dst_time_cls.sum_dst_time+EXCLUDED.sum_dst_time
           ;
       END LOOP;--FOR stat_row
+      curr_oqty:= CASE WHEN obj_exists THEN curr_oqty+obj_row.qty ELSE curr_oqty END;
     END LOOP;--FOR obj_row
     --insert type
     INSERT INTO stat_mov (cid,ctitle,ckind,path,oqty)
@@ -192,7 +99,8 @@ BEGIN
     IF NOT FOUND THEN 
       EXECUTE 'ALTER TABLE pg_temp.stat_mov ADD COLUMN "'||col_name||'" NUMERIC';
     END IF;
-    SELECT sum(qty) INTO curr_oqty FROM obj WHERE cls_id=mov_row.cid;
+    --SELECT sum(qty) INTO curr_oqty FROM obj WHERE cls_id=mov_row.cid;
+    SELECT oqty INTO curr_oqty FROM pg_temp.stat_mov WHERE pg_temp.stat_mov.cid= mov_row.cid;
     curr_perc:= EXTRACT(EPOCH FROM mov_row.sum_dst_time)/ EXTRACT(EPOCH FROM distance)/curr_oqty * 100;
     EXECUTE 'UPDATE pg_temp.stat_mov SET "'
       ||col_name||'"='||round(curr_perc,3)
