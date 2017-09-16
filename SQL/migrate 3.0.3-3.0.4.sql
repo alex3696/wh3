f81cf3afb3b008da7d1fa987005f6d5f2ed6cfde
alex3696 <alex3696@yandex.ru>
2017-09-16 22:33:32 +0500

    SQL: переход на версию БД 3.0.4

--- SQL/migrate 3.0.3-3.0.4.sql
+++ SQL/migrate 3.0.3-3.0.4.sql
@@ -102,15 +102,17 @@ SELECT
   --pt.arr_title[1] AS ctitle,pt.id AS id
   oo.cls_id  AS cid
   ,oo.id AS oid
+  ,acls.title AS ctitle
   ,oo.title AS otitle
   ,jperiod
   FROM obj oo
+  LEFT JOIN acls ON acls.id=oo.cls_id
   LEFT JOIN LATERAL
 ( SELECT
                   json_object_agg( ref_cls_act.act_id, ROUND( (EXTRACT(EPOCH FROM (lastlog.log_dt + ref_cls_act.period -now()) )/86400)::NUMERIC,2 ) ) AS jperiod
                   FROM get_path_cls_info(oo.cls_id, 0) tree
                   INNER JOIN ref_cls_act ON ref_cls_act.cls_id = tree.id  AND ref_cls_act.period IS NOT NULL
-                     AND ( ref_cls_act.act_id =105 OR ref_cls_act.act_id =104 OR ref_cls_act.act_id =108 )
+                     --AND ( ref_cls_act.act_id =105 OR ref_cls_act.act_id =104 OR ref_cls_act.act_id =108 )
                   LEFT JOIN LATERAL(SELECT MAX(timemark) AS log_dt, obj_id AS mobj_id, act_id
                                     --SELECT DISTINCT ON (obj_id,act_id) obj_id AS mobj_id, act_id,timemark AS log_dt  
                                     --SELECT  obj_id AS mobj_id, act_id,timemark AS log_dt  
@@ -129,8 +131,9 @@ SELECT
                   --AND ( ref_cls_act.act_id =105 OR ref_cls_act.act_id =104 OR ref_cls_act.act_id =108 )
    ) AS pp
    ON TRUE
-   WHERE oo.cls_id=257;
-   ---WHERE oo.pid=108;
+   WHERE oo.pid=108;
+   --WHERE oo.cls_id=257;
+   
 */
 
 
@@ -176,11 +179,245 @@ INSERT INTO public.perm_act(access_group, access_disabled, cls_id, src_path, act
 ANALYZE;
 
 -- удалить из таблицы log_detail_act идентификатор действия, т.к. его уже перенесли в log_main
-DROP VIEW log2;
-DROP INDEX idx_logactdet__aid;
+DROP VIEW IF EXISTS log2;
+DROP INDEX IF EXISTS idx_logactdet__aid;
 ALTER TABLE log_detail_act DROP CONSTRAINT log_detail_act_act_id_fkey;
 
---ALTER TABLE log_detail_act DROP COLUMN act_id;
+-----------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+--функция выполнения действия
+-----------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS    do_act(IN _obj_id BIGINT, _act_id BIGINT, IN _prop JSONB) CASCADE;
+
+CREATE OR REPLACE FUNCTION do_act(IN _obj_id BIGINT, _act_id BIGINT, IN _prop JSONB)
+                  RETURNS  VOID AS 
+$BODY$
+DECLARE
+    _lock_info   RECORD;
+    _last_log_id BIGINT;
+    _prop_str    TEXT;
+    _prop_item   TEXT;
+    _curr_pathid BIGINT[];
+    _cls_id      BIGINT;
+    _perm_sum    INTEGER;
+    _old_prop    JSONB;
+
+    _chk_props CURSOR IS
+
+SELECT distinct( ref_act_prop.prop_id) as id, pold.value
+       ,all_new.id as new_id,all_new.value
+       ,CASE WHEN all_new.id IS NOT NULL THEN all_new.value ELSE pold.value END AS result
+       --, COALESCE( all_new.value, pold.value) as result
+  FROM  ref_cls_act
+  RIGHT JOIN ref_act_prop  ON ref_act_prop.act_id  = ref_cls_act.act_id
+  LEFT JOIN (select key::BIGINT, value::TEXT from jsonb_each(_old_prop)) pold ON pold.key=ref_act_prop.prop_id
+  LEFT JOIN(
+             SELECT ref_act_prop.prop_id as id, pnew.value
+             FROM  ref_cls_act
+             RIGHT JOIN ref_act_prop  ON ref_act_prop.act_id  = ref_cls_act.act_id
+             LEFT JOIN (select key::BIGINT, value::TEXT from jsonb_each(_prop)) pnew ON pnew.key=ref_act_prop.prop_id
+             WHERE ref_cls_act.cls_id IN (SELECT id FROM get_path_cls_info(_cls_id)) AND ref_cls_act.act_id=_act_id
+             AND ref_act_prop.prop_id IS NOT NULL 
+            ) all_new ON all_new.id=ref_act_prop.prop_id
+  WHERE ref_cls_act.cls_id IN (SELECT id FROM get_path_cls_info(_cls_id)) 
+  AND ( (all_new.id IS NOT NULL AND all_new.value IS NOT NULL) OR (all_new.id IS NULL  AND pold.value IS NOT NULL)) ;
+
+BEGIN
+  -- проверяем - заблокирован ли объект для действия
+  SELECT * INTO _lock_info 
+    FROM lock_obj
+    LEFT JOIN lock_act USING (oid)
+    WHERE
+      oid = _obj_id
+      AND lock_time +'00:10:00.00' > now()
+      AND lock_session = pg_backend_pid()
+      --AND ((src_path IS NULL AND _src_path IS NULL) OR src_path=_src_path)
+      AND act_id = _act_id;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION ' Object not locked obj_id=% ', _obj_id;
+  END IF;
+  -- находим текущее положение и состояние
+  SELECT cls_id,prop,get_path_obj_arr_2id(pid) INTO _cls_id,_old_prop,_curr_pathid 
+    FROM obj WHERE id=_obj_id;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION ' Object not exists obj_id=% ', _obj_id;
+  END IF;
+  _curr_pathid:=COALESCE(_curr_pathid::TEXT,'{}');
+  RAISE DEBUG '_curr_pathid: %',_curr_pathid;
+  -- проверяем возможность выполнения выбранного действия в текущем местоположении
+    SELECT sum INTO _perm_sum FROM (
+    SELECT perm.act_id , sum(access_disabled) 
+        FROM perm_act perm
+        RIGHT JOIN wh_membership ON groupname=perm.access_group AND wh_membership.username=CURRENT_USER
+      WHERE 
+        perm.cls_id IN(SELECT id FROM get_path_cls_info(_cls_id,0))
+        AND perm.act_id = _act_id
+        AND  (perm.obj_id IS NULL OR perm.obj_id = _obj_id)
+        AND  _curr_pathid::TEXT LIKE perm.src_path
+        --AND perm.access_group IN (SELECT groupname FROM wh_membership WHERE username=CURRENT_USER)
+      GROUP BY act_id
+      ) t 
+      WHERE t.sum=0;
+
+   RAISE DEBUG '_perm_sum: %',COALESCE(_perm_sum,-1);
+
+  IF _perm_sum IS NULL OR _perm_sum>0 THEN
+    RAISE EXCEPTION ' Path was changed for obj_id=% _cls_id=% _act_id=% try again ', _obj_id,_cls_id,_act_id;
+  END IF;
+
+  FOR rec IN _chk_props LOOP
+    _prop_item:= concat_ws(':',quote_ident(rec.id::TEXT), rec.result );
+    _prop_str := concat_ws(',',_prop_str, _prop_item );
+  END LOOP;
+  _prop_str:='{'||_prop_str||'}';
+  RAISE DEBUG '_prop_str: %',_prop_str;
+
+  INSERT INTO log_main(src_path, obj_id,act_id)
+    VALUES ( _curr_pathid, _obj_id, _act_id) RETURNING id INTO _last_log_id;
+  INSERT INTO log_detail_act(id, prop) 
+    VALUES (_last_log_id, _prop_str::JSONB);
+  UPDATE obj_name SET act_logid=_last_log_id, prop=_prop_str::JSONB WHERE id=_obj_id;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE  COST 100;
+
+GRANT EXECUTE ON FUNCTION do_act(IN _obj_id BIGINT, _act_id BIGINT, IN _prop JSONB) TO "User";
+-------------------------------------------------------------------------------
+-- перемещение
+-------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS    do_move( IN _oid  BIGINT, 
+                                    IN _old_opid BIGINT,  IN _new_opid BIGINT, IN _qty NUMERIC ) CASCADE;
+
+CREATE OR REPLACE FUNCTION do_move( IN _oid  BIGINT, 
+                                    IN _old_opid BIGINT,  IN _new_opid BIGINT, IN _qty NUMERIC ) 
+                  RETURNS  VOID AS $BODY$
+DECLARE
+  _lock_info   RECORD;
+  _curr_path BIGINT[];
+  _dst_path BIGINT[];
+  _ckind SMALLINT;
+  _act_logid BIGINT;
+  
+  _src_qty NUMERIC;
+  _dst_qty NUMERIC;
+
+  _move_logid BIGINT;
+BEGIN
+--  определяем заблокирован ли объект для действия
+  SELECT * INTO _lock_info 
+    FROM lock_obj
+    WHERE
+      oid = _oid
+      AND pid = _old_opid
+      AND lock_time +'00:10:00.00' > now()
+      AND lock_session = pg_backend_pid();
+  IF NOT FOUND THEN
+    RAISE EXCEPTION ' Object not locked obj_id=% ', _oid;
+  END IF;
+-- проверяем изменился ли текущий путь объекта
+  _curr_path:=(SELECT get_path_obj_arr_2id(_old_opid));
+  _curr_path:=COALESCE(_curr_path::TEXT,'{}');
+  RAISE DEBUG '_curr_pathid: %',_curr_path;
+  if(_curr_path<>_lock_info.path) THEN
+    RAISE EXCEPTION ' Object src_path was changed obj_id=%, old_path=% curr_path=% '
+                                             , _oid, _lock_info.path,_curr_path;
+  END IF;
+-- проверяем есть ли среди заблокированных пунктов назначений выбранный 
+  _dst_path:=get_path_obj_arr_2id(_new_opid);
+  _dst_path:=COALESCE(_dst_path::TEXT,'{}');
+  PERFORM FROM lock_dst WHERE oid=_oid AND pid=_old_opid AND dst_path = _dst_path 
+                              OR (dst_path IS NULL AND _dst_path IS NULL);
+  IF NOT FOUND THEN
+    RAISE EXCEPTION ' Object dst_path was changed obj_id=%, _dst_path=%'
+                                             , _oid, _dst_path;
+  END IF;
+-- проверяем существование объекта и берём его класс
+  SELECT cls_kind,act_logid,qty INTO _ckind, _act_logid,_src_qty FROM obj WHERE id=_oid AND pid=_old_opid;
+  IF NOT FOUND OR _ckind IS NULL OR _ckind<1 OR _ckind>3 THEN
+    RAISE EXCEPTION ' Object not exist obj_id=%', _oid;
+  END IF;
+-- находим исходное количество и количество в месте назначения
+  SELECT qty INTO _dst_qty FROM obj WHERE pid=_new_opid AND id=_oid;
+  RAISE DEBUG ' _dst_qty=% ',_dst_qty;
+
+  CASE _ckind
+   WHEN 1 THEN PERFORM do_move_objnum(_oid, _new_opid);
+   WHEN 2 THEN PERFORM do_move_objqtyi(_oid, _old_opid, _new_opid, _qty);
+   WHEN 3 THEN PERFORM do_move_objqtyf(_oid, _old_opid, _new_opid, _qty);
+   ELSE RAISE EXCEPTION 'Wrong kind or unknown error'; 
+  END CASE;
+
+  
+
+  
+
+  INSERT INTO log_main(src_path, obj_id, act_id)
+      VALUES (_curr_path, _oid, 0) RETURNING id INTO _move_logid;
+  INSERT INTO log_detail_move(id, dst_path, qty, prop_lid)
+      VALUES (_move_logid,  _dst_path, _qty, _act_logid);
+-- обновляем ссылку на последнее действие в исходном и конечном объектах
+  UPDATE obj_name SET move_logid=_move_logid  WHERE id=_oid;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE  COST 500;
+
+GRANT EXECUTE ON FUNCTION do_move( IN _oid  BIGINT, IN _old_opid BIGINT,  IN _new_opid BIGINT, IN _qty NUMERIC )  TO "User";
+------------------------------------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------------------------------------
+DROP VIEW IF EXISTS log;
+CREATE OR REPLACE VIEW log AS 
+ SELECT 
+  lm.id       AS log_id
+ ,lm.timemark AS log_dt
+ ,lm.username AS log_user
+ ,act.id      AS act_id
+ ,act.title   AS act_title
+ ,act.color   AS act_color
+ ,lm.obj_id   AS mobj_id
+ ,mobj.cls_id AS mcls_id
+ ,mobj.title  AS mobj_title
+ ,mcls.title  AS mcls_title
+ ,CASE WHEN mcls.kind=1 THEN 1::NUMERIC ELSE lmd.qty END AS qty
+ --,CASE WHEN lad.act_id IS NOT NULL THEN lad.prop ELSE NULL::JSONB END AS prop --,det.prop    AS prop
+ ,CASE WHEN lm.act_id<>0 THEN lad.prop ELSE move_prop.prop END AS prop 
+ ,lm.timemark::timestamptz::date  AS log_date
+ ,date_trunc('second' ,lm.timemark)::timestamptz::time AS log_time
+ ,lm.src_path[1][1]  AS src_cid
+ ,lm.src_path[1][2]  AS src_oid
+ ,CASE WHEN lm.act_id<>0 THEN NULL ELSE lmd.dst_path[1][1] END AS dst_cid -- ,det.dst_path[1][1] AS dst_cid
+ ,CASE WHEN lm.act_id<>0 THEN NULL ELSE lmd.dst_path[1][2] END AS dst_oid -- ,det.dst_path[1][2] AS dst_oid
+ ,lm.src_path        AS src_ipath
+ ,CASE WHEN lm.act_id<>0 THEN NULL ELSE lmd.dst_path END AS dst_ipath --,det.dst_path       AS dst_ipath
+ ,(SELECT path FROM tmppath_to_2id_info(lm.src_path::TEXT,1)) AS src_path
+ ,(SELECT path FROM tmppath_to_2id_info(lmd.dst_path::TEXT,1)) AS dst_path
+
+ ,lmd.prop_lid AS prop_lid
+ ,mcls.kind    AS mcls_kind
+ ,mcls.measure AS mcls_measure
+FROM log_main lm
+  LEFT JOIN log_detail_act  lad ON lad.id=lm.id
+  LEFT JOIN log_detail_move lmd ON lmd.id=lm.id
+  LEFT JOIN log_detail_act move_prop ON lmd.prop_lid=move_prop.id
+
+  LEFT JOIN act ON act.id=lm.act_id
+  LEFT JOIN obj_name mobj ON mobj.id=lm.obj_id
+  LEFT JOIN acls     mcls ON mcls.id=mobj.cls_id
+  ;
+
+GRANT SELECT        ON log  TO "Guest";
+GRANT DELETE        ON log  TO "User";
+
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+CREATE TRIGGER tr_bd_log INSTEAD OF DELETE ON public.log FOR EACH ROW EXECUTE PROCEDURE public.ftg_del_log();
+
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+ALTER TABLE log_detail_act DROP COLUMN act_id;
 
 
 
