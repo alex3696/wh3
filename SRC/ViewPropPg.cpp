bbc9c2950f213447362a4836ffab3f9b0848f58b
alex3696 <alex3696@yandex.ru>
2020-02-26 16:06:14 +0500

    fix\CPP - добавить коммит свойств перед выполнением действия, иначе при вставке теряются значения

--- SRC/ViewPropPg.cpp
+++ SRC/ViewPropPg.cpp
@@ -104,6 +104,7 @@ void ViewPropPg::SetAfterRefresh(std::shared_ptr<const ModelPropTable> table)
 //-----------------------------------------------------------------------------
 void ViewPropPg::GetPropValues(std::map<int64_t, wxString>& arr)
 {
+	mPG->EditorsValueWasModified();
 	mPG->CommitChangesFromEditor();
 	wxPropertyGridIterator it;
 
